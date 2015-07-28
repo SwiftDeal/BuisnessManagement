d2ff0d98a88e1596ddfd314f7b25b6e42046614b
faizanayubi <indianayubi@gmail.com>
2015-07-29 04:21:10 +0530

    Added Routes

--- public/routes.php
+++ public/routes.php
@@ -4,14 +4,24 @@
 
 $routes = array(
     array(
-        "pattern" => "features",
+        "pattern" => "about",
         "controller" => "home",
-        "action" => "features"
+        "action" => "about"
     ),
     array(
         "pattern" => "home",
         "controller" => "home",
         "action" => "index"
+    ),
+    array(
+        "pattern" => "work",
+        "controller" => "home",
+        "action" => "work"
+    ),
+    array(
+        "pattern" => "team",
+        "controller" => "home",
+        "action" => "team"
     )
 );
 
