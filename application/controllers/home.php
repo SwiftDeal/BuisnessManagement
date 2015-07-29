ce23cea71237c49f00a7a4f0f32a04e63b0b4940
faizanayubi <indianayubi@gmail.com>
2015-07-30 01:30:32 +0530

    Added SEO Plugin

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -5,28 +5,40 @@
  *
  * @author Faizan Ayubi
  */
-use Framework\Controller as Controller;
+use Shared\Controller as Controller;
 
 class Home extends Controller {
 
     public function index() {
-        
+        $this->getLayoutView()->set("seo", Framework\Registry::get("seo"));
     }
     
     public function about() {
-        
+        $this->seo(array(
+            "title" => "About Us",
+            "view" => $this->getLayoutView()
+        ));
     }
     
     public function work() {
-        
+        $this->seo(array(
+            "title" => "Our Work",
+            "view" => $this->getLayoutView()
+        ));
     }
     
     public function team() {
-        
+        $this->seo(array(
+            "title" => "Our Team",
+            "view" => $this->getLayoutView()
+        ));
     }
     
     public function contact() {
-        
+        $this->seo(array(
+            "title" => "Contact",
+            "view" => $this->getLayoutView()
+        ));
     }
 
 }
