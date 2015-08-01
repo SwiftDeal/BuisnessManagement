c5ed1b679498689d9d8c3d6521d38de9d5e8af49
faizanayubi <indianayubi@gmail.com>
2015-08-02 03:32:42 +0530

    Added Accounts and HR Model

--- application/models/user.php
+++ application/models/user.php
@@ -10,10 +10,13 @@ class User extends Shared\Model {
     /**
      * @column
      * @readwrite
-     * @primary
-     * @type autonumber
+     * @type text
+     * @length 100
+     * 
+     * @validate required, min(3), max(100)
+     * @label name
      */
-    protected $_id;
+    protected $_name;
 
     /**
      * @column
@@ -22,20 +25,9 @@ class User extends Shared\Model {
      * @length 100
      * 
      * @validate required, alpha, min(3), max(32)
-     * @label first name
+     * @label phone number
      */
-    protected $_first;
-
-    /**
-     * @column
-     * @readwrite
-     * @type text
-     * @length 100
-     * 
-     * @validate required, alpha, min(3), max(32)
-     * @label last name
-     */
-    protected $_last;
+    protected $_phone;
 
     /**
      * @column
@@ -60,12 +52,4 @@ class User extends Shared\Model {
      * @label password
      */
     protected $_password;
-    
-    /**
-    * @column
-    * @readwrite
-    * @type boolean
-    */
-    protected $_admin = false;
-
 }
