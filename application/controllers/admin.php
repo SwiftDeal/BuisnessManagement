1355b2294f16ff59760e3b9c599d90a0534ee465
faizanayubi <indianayubi@gmail.com>
2015-08-08 11:33:30 +0530

    Added Login Method

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -6,7 +6,29 @@
  * @author Faizan Ayubi
  */
 use Shared\Controller as Controller;
+use Framework\RequestMethods as RequestMethods;
 
-class Admin extends Controller{
-    //put your code here
+class Admin extends Controller {
+    
+    public function index() {
+        $this->seo(array("title" => "Dashboard", "view" => $this->getLayoutView()));
+        
+    }
+    
+    public function login() {
+        $this->defaultLayout = "layouts/blank";
+        $this->setLayout();
+        $this->seo(array("title" => "Login", "view" => $this->getLayoutView()));
+        
+        if(RequestMethods::post("action") == "login") {
+            $user = User::first(array(
+                "email = ?" => RequestMethods::post("email"),
+                "password = ?" => RequestMethods::post("password")
+            ));
+            if ($user) {
+                
+            }
+        }
+    }
+    
 }
