d4b727a9f98ebd3ad4f9986712a97747e1a88dae
faizanayubi <indianayubi@gmail.com>
2015-08-08 19:08:46 +0530

    Login and Register Completed

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -9,26 +9,67 @@ use Shared\Controller as Controller;
 use Framework\RequestMethods as RequestMethods;
 
 class Admin extends Controller {
-    
+
     public function index() {
         $this->seo(array("title" => "Dashboard", "view" => $this->getLayoutView()));
-        
+        $view = $this->getActionView();
     }
-    
+
+    public function sync($model) {
+        $this->noview();
+        $db = Framework\Registry::get("database");
+        $db->sync(new $model);
+    }
+
     public function login() {
         $this->defaultLayout = "layouts/blank";
         $this->setLayout();
         $this->seo(array("title" => "Login", "view" => $this->getLayoutView()));
-        
-        if(RequestMethods::post("action") == "login") {
+        $view = $this->getActionView();
+
+        if (RequestMethods::post("action") == "login") {
             $user = User::first(array(
-                "email = ?" => RequestMethods::post("email"),
-                "password = ?" => RequestMethods::post("password")
+                        "email = ?" => RequestMethods::post("email"),
+                        "password = ?" => sha1(RequestMethods::post("password")),
+                        "validity" => TRUE
             ));
             if ($user) {
-                
+                $members = Member::all(array("user_id = ?" => $user->id));
+                $this->session($user, $members);
+                self::redirect("/admin");
+            } else {
+                $view->set("message", "User not exist or blocked");
             }
         }
     }
-    
+
+    protected function session($user, $members) {
+        $this->setUser($user);
+        Registry::get("session")->set("members", $members);
+    }
+
+    public function register() {
+        $this->defaultLayout = "layouts/blank";
+        $this->setLayout();
+        $this->seo(array("title" => "Register", "view" => $this->getLayoutView()));
+        $view = $this->getActionView();
+
+        if (RequestMethods::post("action") == "register") {
+            $exist = User::first(array("email = ?" => RequestMethods::post("email")));
+            if (!$exist) {
+                $user = new User(array(
+                    "name" => RequestMethods::post("name"),
+                    "email" => RequestMethods::post("email"),
+                    "password" => sha1(RequestMethods::post("password")),
+                    "phone" => RequestMethods::post("phone"),
+                    "validity" => FALSE
+                ));
+                $user->save();
+                $view->set("message", "Your account has been created contact HR to activate");
+            } else {
+                $view->set("message", 'Account exists, login from <a href="/admin/login">here</a>');
+            }
+        }
+    }
+
 }
